bdb8b6a229968e41489004193712a2738da6ac28
artsemlemesh <126878429+artsemlemesh@users.noreply.github.com>
2025-01-14 08:54:00 +0800

    test

--- src/tests/Hello.test.jsx
+++ src/tests/Hello.test.jsx
@@ -1,8 +1,6 @@
-// src/components/__tests__/Hello.test.js
 import React from 'react';
-import { render, screen } from '@testing-library/react';
 import Hello from './Hello';
-
+import {render, screen} from '@testing-library/react';
 test('renders Hello component with name', () => {
   render(<Hello name="World" />);
   const headingElement = screen.getByText(/Hello, World!/i);
