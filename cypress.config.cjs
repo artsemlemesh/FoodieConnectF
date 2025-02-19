222d7a23756ab1b1217abecec3ab5f9cad08539f
artsemlemesh <126878429+artsemlemesh@users.noreply.github.com>
2025-02-19 08:50:23 +0800

    updated cypress

---
+++ cypress.config.cjs
@@ -0,0 +1,13 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      return config;
+    },
+    
+    specPattern: 'cypress/e2e/**/*.{js,jsx}',
+    baseUrl: 'http://localhost:3001', // Replace with your app's base URL
+  },
+});
