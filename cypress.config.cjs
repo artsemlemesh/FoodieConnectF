56e378421e780c709d89625540ff482130a2d6ff
artsemlemesh <126878429+artsemlemesh@users.noreply.github.com>
2025-02-23 10:15:07 +0800

    d

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -10,6 +10,8 @@ module.exports = defineConfig({
     specPattern: 'cypress/e2e/**/*.{js,jsx}',
     baseUrl: 'http://localhost:3001', // Replace with your app's base URL
     video: true, // Records test runs
+    screenshotsFolder: "cypress/screenshots",
+    videosFolder: "cypress/videos",
     screenshotOnRunFailure: true, // Saves a screenshot if a test fails
   },
 });
