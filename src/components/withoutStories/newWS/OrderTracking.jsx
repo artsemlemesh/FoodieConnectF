3b73b8ab0ed2ac9fd6cc4e18162df70297ec10af
artsemlemesh <126878429+artsemlemesh@users.noreply.github.com>
2025-03-01 13:59:34 +0800

    component change

--- src/components/withoutStories/newWS/OrderTracking.jsx
+++ src/components/withoutStories/newWS/OrderTracking.jsx
@@ -1,40 +1,63 @@
-import { useEffect, useState } from "react";
-import { axiosClient } from "../../../utils/axiosClient";
-import DeliveryMap from "./DeliveryMap";
+import { useEffect, useState } from 'react';
+import { axiosClient } from '../../../utils/axiosClient';
+import DeliveryMap from './DeliveryMap';
 
 const OrderTracking = ({ orderId }) => {
   const [order, setOrder] = useState(null);
+  console.log('OrderTracking orderId:', orderId);
+
+  const fetchOrder = async () => {
+    try {
+      const response = await axiosClient.get(
+        `http://localhost:8000/livedel/orders/${orderId}/`
+      );
+
+      console.log('get response:', response.data);
+      setOrder(response.data);
+    } catch (error) {
+      console.error('Error fetching order:', error);
+    }
+  };
 
   useEffect(() => {
-    const fetchOrder = async () => {
+    const startDelivery = async () => {
       try {
-        const response = await axiosClient.get(
-          `http://localhost:8000/livedel/orders/${orderId}/`
+        const response = await axiosClient.post(
+          `http://localhost:8000/livedel/orders/${orderId}/start-delivery/`
         );
 
-        console.log("Order response:", response.data);
-        setOrder(response.data);
+        console.log('post response:', response.data);
+        // setOrder(response.data);
       } catch (error) {
-        console.error("Error fetching order:", error);
+        console.error('Error fetching order:', error);
       }
     };
+
+    startDelivery();
     fetchOrder();
+
+
+    // Polling: Fetch order updates every 5 seconds
+    const interval = setInterval(fetchOrder, 5000);
+
+    return () => clearInterval(interval); // Cleanup on unmount
+
   }, [orderId]);
 
   if (!order) {
     return <p>Loading order details...</p>;
   }
 
-  console.log("Order:", order);
+  console.log('Order:', order);
   return (
     <div>
       <h2>Order #{orderId} Tracking</h2>
       <p>Status: {order.status}</p>
       <p>
-        ETA:{" "}
+        ETA:{' '}
         {order.eta
           ? new Date(order.eta).toLocaleTimeString()
-          : "Calculating..."}
+          : 'Calculating...'}
       </p>
 
       <DeliveryMap
